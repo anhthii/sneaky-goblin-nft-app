5b55ee5666085097e5c28dab8ff2b5e26742a08b
Valentin Politov <valentin@smartdata.solutions>
2022-04-19 17:53:03 +0300

    Added How To Play text

--- src/components/pages/_layouts/HowToPlay/HowToPlay.jsx
+++ src/components/pages/_layouts/HowToPlay/HowToPlay.jsx
@@ -1,54 +1,91 @@
 import React from 'react';
+import './HowToPlay.scss';
 
 const HowToPlay = () => {
     return (
-        <>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum. laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum. laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaec.
-            </p>
-            <br />
-            <p>
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum. laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                officia deserunt mollit anim id est laborum. laboris nisi ut aliquip ex ea commodo
-                consequat.
-            </p>
-            <br />
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum. laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum. laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaec.
-            </p>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-        </>
+        <dl className="_how-to-play">
+            <dt>The Goblinverse Invasion:</dt>
+            <dd>
+                By staking your Sneaky Goblins into the Goblinverse Invasion Campaign, you send your
+                Goblins team to fight for $xSERUM as invaders.
+            </dd>
+            <dd>
+                There are 5 different levels of Goblins that reflects their skills, brain power and
+                cleverness. The level directly affects the $xSERUM yield. Higher levels are higher
+                rarity.
+            </dd>
+
+            <dt>What’s the early benefits?</dt>
+            <dd>
+                Before reveal, all Sneaky Goblins will yield the same amount of $xSERUM based on
+                level 1 yield. However, if you stake your Goblins from day 1 and all the way through
+                the reveal, the yielded $xSERUM in your vault will update according to the REAL
+                level of your Sneaky Goblin.
+                <br /> Example: if you’ve been yielding 100 $xSERUM per day as a non-revealed Goblin
+                and then your Goblin ends up being a Level 5, your $xSERUM in your vault will be
+                updated based on level 5 yield RETROACTIVELY.
+                <br />
+                Requirement: The only requirement is that you keep your Goblin staked until reveal.
+            </dd>
+
+            <dt>What’s the daily $xSERUM yield?</dt>
+            <dd>
+                <table>
+                    <thead>
+                        <tr>
+                            <th scope="col">Category</th>
+                            <th scope="col">Yield/day</th>
+                            <th scope="col">
+                                Rarity <br />
+                                (% of occurrence)
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Level 1</th>
+                            <td>100</td>
+                            <td>35%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Level 2</th>
+                            <td>120</td>
+                            <td>25%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Level 3</th>
+                            <td>140</td>
+                            <td>20%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Level 4</th>
+                            <td>150</td>
+                            <td>15%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Level 5</th>
+                            <td>200</td>
+                            <td>5%</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </dd>
+
+            <dt>What wallet balance is used for future drops &amp; ecosystem spending?</dt>
+            <dd>
+                It’s the in-game wallet balance that is used to mint upcoming drops and to spend
+                $xSERUM in our Goblinverse Ecosystem.
+                <br />
+                If you withdraw to ERC-20, you’ll need to re-deposit to be able to benefit from the
+                ecosystem.
+            </dd>
+
+            <dt>What happens if I claim $xSERUM from in-game wallet to ERC-20?</dt>
+            <dd>
+                You can claim from in-game wallet to ERC-20 at any time. However, keep in mind that
+                there is a 25% tax to the Goblin Nation Government doing so.
+            </dd>
+        </dl>
     );
 };
 
